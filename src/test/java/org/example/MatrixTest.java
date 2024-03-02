f3f68bb2a612eeeadf783e712dff1345f771c7a8
Narcisse CS <narcissepotato5@gmail.com>
2024-03-02 16:41:40 +0100

    lol

---
+++ src/test/java/org/example/MatrixTest.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatrixTest {
+
+    @Test
+    void set() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void multiply() {
+    }
+
+    @Test
+    void transpose() {
+    }
+}
